49a7cf3c5540b09822957bdb516dcc313624179e
adman <365041504@qq.com>
2019-08-06 21:46:30 +0800

    本次修该的内容

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -5,6 +5,6 @@ public class Main {
     @Test
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
-        System.out.println("在次提交到新的代码到服务器");
+        System.out.println("再次提交新东西");
     }
 }
