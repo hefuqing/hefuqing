25b80106d72291397f7417ae76a1327c9b6f90ec
adman <365041504@qq.com>
2019-08-06 21:37:47 +0800

    李四提交

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -6,5 +6,6 @@ public class Main {
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
         System.out.println("在次提交到新的代码到服务器");
+		 System.out.println("李四提交到新的代码到服务器");
     }
 }
